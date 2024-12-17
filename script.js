c609bf9b76e5ea9b209783395fac7ad1bdf12732
Yagor <yegor.dyakovskiy@gmail.com>
2024-12-17 08:03:04 +0500

    add adaptive

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function loadAllContent() {
     //  await loadHTML('side-banners__content', './html/advantages.html');
     //  await loadHTML('side-banners__content', './html/socials.html');
     //  await loadHTML('side-banners__content', './html/video.html');
-   // await loadHTML('side-banners__content', './html/gallery.html');
+    // await loadHTML('side-banners__content', './html/gallery.html');
     //   await loadHTML('side-banners__content', './html/services.html');
     //  await loadHTML('side-banners__content', './html/socials-banner.html');
     // await loadHTML('side-banners__content', './html/form.html');
